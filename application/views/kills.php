94b0e600c98a6d9f80af68535d1b8e4bc9f9554f
John Schavey <jschavey@me.com>
2014-08-27 08:48:09 -0600

    updates for broken paragon api

--- application/views/kills.php
+++ application/views/kills.php
@@ -1,25 +1,25 @@
 <div class="panel panel-default">
 	<div class="panel-heading">
 	  	<h1 class="panel-title" data-toggle="collapse" data-target="#kills">
-   			Lifetime Kills: <?= number_format( $kills ) ?>
-   		</h1>
-   	</div>
+   		Lifetime Kills: <?= number_format( $kills ) ?>
+   	</h1>
+   </div>
 
-   	<div id="kills" class="collapse panel-body">
-   		<? foreach( $careers as $career ) : ?>
-   			<div class="row">
-   				<div class="col-md-2">
-   					<?= $career->battleTag ?>
-	   			</div>
+   <div id="kills" class="collapse panel-body">
+   	<? foreach( $careers as $career ) : ?>
+   		<div class="row">
+   			<div class="col-md-2">
+   				<?= $career->battleTag ?>
+	   		</div>
    				
-   				<div class="col-md-10">
-		   			<div class="progress">
+   			<div class="col-md-10">
+		   		<div class="progress">
 						<div class="progress-bar" role="progressbar" aria-valuenow="<?= $career->kills->monsters / $kills * 100 ?>" aria-valuemin="0" aria-valuemax="100" style="width: <?= $career->kills->monsters / $kills * 100 ?>%;">
 						    <span class="sr-only"><?= $career->kills->monsters / $kills * 100 ?>% Complete</span>
 						</div>
 					</div>
-   				</div>
    			</div>
-   		<? endforeach ?>
+   		</div>
+   	<? endforeach ?>
 	</div>
 </div>
--- application/views/paragon.php
+++ application/views/paragon.php
@@ -6,22 +6,24 @@
    </div>
 
    <div id="paragon" class="collapse panel-body">
-      <div id="body">
-      		<? foreach( $careers as $career ) : ?>
-      			<div class="row">
-      				<div class="col-md-2">
-                     <?= $career->battleTag ?>
-                  </div>
-      				
-      				<div class="col-md-10">
-   		   			<div class="progress">
-   						<div class="progress-bar" role="progressbar" aria-valuenow="<?= $career->paragonLevel / $paragon * 100 ?>" aria-valuemin="0" aria-valuemax="100" style="width: <?= $career->paragonLevel / $paragon * 100 ?>%;">
-   						    <span class="sr-only"><?= $career->paragonLevel / $paragon * 100 ?>% Complete</span>
-   						</div>
-   					</div>
-      				</div>
-      			</div>
-      		<? endforeach ?>
+      <div class="alert alert-danger" role="alert">
+         Paragon API broken by 2.1 patch
       </div>
+
+      <? foreach( $careers as $career ) : ?>
+      	<div class="row">
+      		<div class="col-md-2">
+               <?= $career->battleTag ?>
+            </div>
+      				
+      		<div class="col-md-10">
+   	  			<div class="progress">
+      				<div class="progress-bar" role="progressbar" aria-valuenow="<?= ( $paragon ) ? ($career->paragonLevel / $paragon * 100) : 0 ?>" aria-valuemin="0" aria-valuemax="100" style="width: <?= ( $paragon ) ? ($career->paragonLevel / $paragon * 100) : 0 ?>%;">
+          			   <span class="sr-only"><?= ( $paragon ) ? ($career->paragonLevel / $paragon * 100) : 0 ?>% Complete</span>
+      				</div>
+   				</div>
+      		</div>
+      	</div>
+      <? endforeach ?>
    </div>
 </div>
